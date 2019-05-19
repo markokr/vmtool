e7a09c3955f4c40704cf1a91b8841a8be71547ec
Marko Kreen <markokr@gmail.com>
2019-05-19 19:06:54 +0300

    Allow aliases to specify command list instead of role list.

--- vmtool/run.py
+++ vmtool/run.py
@@ -13,6 +13,8 @@ from vmtool.envconfig import load_env, load_env_config
 
 
 def run_command(cf, args):
+    """Command is implemented by class specified in vmtool_profile.
+    """
     mod_name = cf.get('vmtool_profile')
     mod = importlib.import_module(mod_name)
 
@@ -22,18 +24,41 @@ def run_command(cf, args):
     sys.stderr.flush()
 
 
-def run_alias(env_name, alias, cmd, cmdpos, args, options):
+def run_alias(env_name, alias, cmd, cmdpos, args, options, is_cmd):
+    """Alias that launches different other commands.
+
+    It cannot launch other aliases.
+
+    Example - run "foo" on role "this" and "foox" on "other"::
+
+        [alias.foo]
+        roles = this, other:foox
+
+    Example - run "foo" on current role and "foox" on "other"::
+
+        [alias.foo]
+        commands = foo, other:foox
+
+    """
     cmd_prefix = args[:cmdpos]
     cmd_self = args[cmdpos:cmdpos+1]
     cmd_suffix = args[cmdpos+1:]
-    for role in alias.split(','):
-        role = role.strip()
-        xcmd = cmd_self
-        if ':' in role:
-            role, acmd = role.split(':', 1)
+    for elem in alias.split(','):
+        elem = elem.strip()
+        if ':' in elem:
+            role, acmd = elem.split(':', 1)
             role, acmd = role.strip(), acmd.strip()
             xcmd = shlex.split(acmd)
-        xargs = ['--role=' + role] + cmd_prefix + xcmd + cmd_suffix
+        elif is_cmd:
+            xcmd = shlex.split(elem)
+            role = None
+        else:
+            xcmd = cmd_self
+            role = elem
+
+        xargs = cmd_prefix + xcmd + cmd_suffix
+        if role:
+            xargs = ['--role=' + role] + xargs
 
         extra = ''
         if options:
@@ -48,6 +73,8 @@ def run_alias(env_name, alias, cmd, cmdpos, args, options):
 
 
 def main():
+    """Parse command-line, run commands.
+    """
     args = sys.argv[1:]
 
     # parse command
@@ -69,11 +96,16 @@ def main():
     # does role need replacing
     alias_sect = 'alias.%s' % cmd
     if cmd and cf.has_section(alias_sect):
-        alias = cf.cf.get(alias_sect, 'roles')
+        if cf.cf.has_option(alias_sect, 'roles'):
+            alias = cf.cf.get(alias_sect, 'roles')
+            is_cmd = False
+        else:
+            alias = cf.cf.get(alias_sect, 'commands')
+            is_cmd = True
         options = []
         if cf.cf.has_option(alias_sect, 'options'):
             options = shlex.split(cf.cf.get(alias_sect, 'options'))
-        run_alias(env_name, alias, cmd, cmdpos, args, options)
+        run_alias(env_name, alias, cmd, cmdpos, args, options, is_cmd)
     else:
         run_command(cf, args)
 
