536b4924b09291acafc123f649267cf5794d09fe
Tanel Andreson <tanel.andreson@veriff.net>
2026-03-23 10:30:17 +0200

    avoid duplicate data in the tarball

--- vmtool/tarball.py
+++ vmtool/tarball.py
@@ -24,6 +24,7 @@ class TarBall(object):
     def __init__(self):
         self.buf = io.BytesIO()
         self.tf = tarfile.open('buf.tgz', 'w|gz', self.buf, format=tarfile.PAX_FORMAT)
+        self.added_paths = set()  # Track added paths to avoid duplicates
 
     def filter_data(self, fname, data):
         """Overridable function."""
@@ -56,6 +57,11 @@ class TarBall(object):
         fpath, data = self.filter_data(fpath, data)
         if not fpath:
             return
+
+        # Skip if already added
+        if fpath in self.added_paths:
+            return
+
         if data is not origdata:
             mtime = None
         inf = tarfile.TarInfo(fpath)
@@ -78,6 +84,7 @@ class TarBall(object):
         inf.size = len(data)
 
         self.tf.addfile(inf, io.BytesIO(data))
+        self.added_paths.add(fpath)
 
     def add_dir(self, dpath, mode=TAR_DIR_MODE, mtime=None):
         """Add directory entry."""
@@ -86,7 +93,14 @@ class TarBall(object):
         if not dpath:
             return
 
-        inf = tarfile.TarInfo(dpath + '/')
+        # Normalize directory path with trailing slash for tracking
+        dir_entry = dpath + '/'
+
+        # Skip if already added
+        if dir_entry in self.added_paths:
+            return
+
+        inf = tarfile.TarInfo(dir_entry)
         inf.mtime = mtime or time.time()
         inf.uid = 1000
         inf.gid = 1000
@@ -95,6 +109,7 @@ class TarBall(object):
         inf.mode = mode
         inf.type = tarfile.DIRTYPE
         self.tf.addfile(inf)
+        self.added_paths.add(dir_entry)
 
     def close(self):
         """Close tarball."""
